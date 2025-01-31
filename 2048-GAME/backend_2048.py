5e339f632c00ebb1ca21d7122a9b427273009f79
Even00002 <pd51emw@eduvaud.ch>
2025-01-31 15:13:49 +0100

    Add files via upload

---
+++ 2048-GAME/backend_2048.py
@@ -0,0 +1,20 @@
+
+
+cases= [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]
+]
+
+game= [
+        [2,4,8,],
+        [16,32,64],
+        [128,256,512],
+        [1024,2048,4096],
+        [8192]
+]
+
+labels=[[None,None,None],[None,None,None],[None,None,None]]
+
+dx=10 # distance horizontal entre les labels
+dy=10 # distance vertical entre les labels
